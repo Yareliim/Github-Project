8acd13c5f224d76b22ab48033aded47542d2d57a
Yareliim <152998525+Yareliim@users.noreply.github.com>
2023-12-06 19:34:44 -0600

    Revert "Update #2: Lab7b.cpp"

--- Lab7b.cpp
+++ Lab7b.cpp
@@ -7,10 +7,10 @@ int calculateValue(const string& word)
 {
 	int pointValue = 0;
 
-	for (char letter : word)
+	for (int i = 0; i < word.length(); i++)
 	{
 
-		
+		char letter = i;
 		switch (letter) {
 		case 'A':
 		case 'E':
@@ -59,7 +59,7 @@ int calculateValue(const string& word)
 			pointValue += 10;
 			break;
 
-			
+			default;
 		}
 
 
